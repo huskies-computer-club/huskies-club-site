5a9f79aa26522580a8262e3568698544eaadbad3
Luis Martinez <mrluismartinezzz@gmail.com>
2023-11-03 15:43:37 -0700

    installs sentry.io

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,23 @@
 	import discordImg from '$lib/images/discord-logo-black.png';
 	import './styles.css';
 	import '../app.css';
+	import * as Sentry from '@sentry/svelte';
+	Sentry.init({
+		dsn: 'https://5dba0ffb4221d49b21b5d5800fc52d8c@o4506164319289344.ingest.sentry.io/4506164324335616',
+		integrations: [
+			new Sentry.BrowserTracing({
+				// Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+				tracePropagationTargets: ['localhost', 'huskiestech.netlify.app']
+			}),
+			new Sentry.Replay()
+		],
+
+		// Performance Monitoring
+		tracesSampleRate: 1.0, // Capture 100% of the transactions
+		// Session Replay
+		replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+		replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+	});
 </script>
 
 <div class="app" id="app">
